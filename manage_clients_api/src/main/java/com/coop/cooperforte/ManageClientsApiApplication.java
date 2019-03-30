575d9d81d99ce035ef22fad9a569b22099502aea
Felipe Marques <fmarques899@gmail.com>
2019-03-29 21:21:10 -0300

    Creating repository with client and server projects

---
+++ manage_clients_api/src/main/java/com/coop/cooperforte/ManageClientsApiApplication.java
@@ -0,0 +1,13 @@
+package com.coop.cooperforte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageClientsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageClientsApiApplication.class, args);
+	}
+
+}
---
+++ manage_clients_api/src/test/java/com/coop/cooperforte/ManageClientsApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coop.cooperforte;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManageClientsApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
